a604a01d87a382aa7687c6b69c0585052d5dec0c
Leo Herrick <leoherrick@gmail.com>
2010-11-03 07:48:24 -0700

    moved setup to bottom of domino_group_spec

--- spec/domino_group_spec.rb
+++ spec/domino_group_spec.rb
@@ -3,22 +3,9 @@ require "spec_helper"
 module DominoMofo
 
   describe DominoGroup do
-    
-    before(:each) do
-      @standard_set_of_dominoes = DominoGroup.new
-      @standard_set_of_dominoes.push(
-       Double.new(0), Domino.new(0,1), Domino.new(0,2), Domino.new(0,3), Domino.new(0,4), Domino.new(0,5), Domino.new(0,6),
-       Double.new(1), Domino.new(1,2), Domino.new(1,3), Domino.new(1,4), Domino.new(1,5), Domino.new(1,6),
-       Double.new(2), Domino.new(2,3), Domino.new(2,4), Domino.new(2,5), Domino.new(2,6),
-       Double.new(3), Domino.new(3,4), Domino.new(3,5), Domino.new(3,6), 
-       Double.new(4), Domino.new(4,5), Domino.new(4,6),
-       Double.new(5), Domino.new(5,6),
-       Double.new(6)
-      )
-    end
+    before(:each) { @standard_set_of_dominoes = create_standard_set_of_dominoes }
 
     describe "#has_domino_with_suits" do
-
       it "should return true if group has dominoes with the given suits" do
         @standard_set_of_dominoes.should have_domino_with_suits(6,6)
         @standard_set_of_dominoes.should have_domino_with_suits(1,0)
@@ -53,7 +40,6 @@ module DominoMofo
     end
     
     describe "#remove_domino_with_suits!" do
-    
       it "should remove domino with given ends from the group and return that group" do
         expect {
           @standard_set_of_dominoes.remove_domino_with_suits!(6,6)
@@ -61,6 +47,19 @@ module DominoMofo
           @standard_set_of_dominoes.has_domino_with_suits?(6,6)
         }.from(true).to(false)
       end
-    end    
+    end        
+  end
+  def create_standard_set_of_dominoes
+    standard_set_of_dominoes = DominoGroup.new
+    standard_set_of_dominoes.push(
+     Double.new(0), Domino.new(0,1), Domino.new(0,2), Domino.new(0,3), Domino.new(0,4), Domino.new(0,5), Domino.new(0,6),
+     Double.new(1), Domino.new(1,2), Domino.new(1,3), Domino.new(1,4), Domino.new(1,5), Domino.new(1,6),
+     Double.new(2), Domino.new(2,3), Domino.new(2,4), Domino.new(2,5), Domino.new(2,6),
+     Double.new(3), Domino.new(3,4), Domino.new(3,5), Domino.new(3,6), 
+     Double.new(4), Domino.new(4,5), Domino.new(4,6),
+     Double.new(5), Domino.new(5,6),
+     Double.new(6)
+    )   
+    standard_set_of_dominoes
   end
 end
